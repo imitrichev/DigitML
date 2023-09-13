61c26df6b2fbe377541f89c373d2296ef2bc8659
EugeneStr <99639852+EugeneStr@users.noreply.github.com>
2023-09-13 13:49:48 +0300

    Update max function

--- src/NN.cpp
+++ src/NN.cpp
@@ -185,7 +185,7 @@ std::vector<double> NeuralNetwork::sigmoid(const std::vector<double>& x) {
     return result;
 }
 
-double max(int a, int b) {
+double max(double a, double b) {
     if (a >= b)
         return a;
     else
