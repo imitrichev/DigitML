3154acf8abb3fae9fa2650c62a37b3da4a36ae30
EugeneStr <99639852+EugeneStr@users.noreply.github.com>
2023-09-13 14:08:32 +0300

    Add end_to_end_test function

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ const double calculate_accuracy(const Matrix<unsigned char>& images, const Matri
   return accuracy;
 }
 
-int main() {
+void end_to_end_test(){
     Matrix<unsigned char> images_train(0, 0);
     Matrix<unsigned char> labels_train(0, 0);
     load_dataset(images_train, labels_train, "data/train-images-idx3-ubyte", "data/train-labels-idx1-ubyte");
@@ -46,25 +46,6 @@ int main() {
 
     NeuralNetwork n;
 
-    // Tests to see that data was read in properly
-    /*for (int i = 0; i < 10; ++i) {
-        Example e;
-        for (int j = 0; j < 28*28; ++j) {
-            e.data[j] = images_train[i][j];
-        }
-        e.label = labels_train[i][0];
-        debug(e);
-        printf("Guess: %d\n", n.compute(e));
-    }
-    for (int i = 0; i < 10; ++i) {
-        Example e;
-        for (int j = 0; j < 28*28; ++j) {
-            e.data[j] = images_test[i][j];
-        }
-        e.label = labels_test[i][0];
-        debug(e);
-        printf("Guess: %d\n", n.compute(e));
-    }*/
     const unsigned int num_iterations = 5;
     n.train(num_iterations, images_train, labels_train);
 
@@ -72,7 +53,11 @@ int main() {
     const double accuracy_test = calculate_accuracy(images_test, labels_test, n);
 
     printf("Accuracy on training data: %f\n", accuracy_train);
-    printf("Accuracy on test data: %f\n", accuracy_test);
+    printf("Accuracy on test data: %f\n", accuracy_test
+}
 
+int main() {
+    for(i=0;i<5;i++){
+        end_to_end_test();
     return 0;
 }
