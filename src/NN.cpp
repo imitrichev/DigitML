40bd7023627b09dd3e5666f6446d46a2ffcc6989
Browwniass <95659478+Browwniass@users.noreply.github.com>
2023-09-25 21:47:46 +0300

    9

--- src/NN.cpp
+++ src/NN.cpp
@@ -141,7 +141,7 @@ void NeuralNetwork::compute_gradients_and_cost(
 inline std::vector<double> NeuralNetwork::feed_forward(
         const std::vector<double>& input,
         const Matrix<double>& weights) {
-    return personal_sigmoid(weights * input);
+    return sigmoid(weights * input);
 }
 
 Matrix<double> NeuralNetwork::weight_init(double maxWeight, unsigned int rows, unsigned int cols){
@@ -182,7 +182,7 @@ unsigned int NeuralNetwork::compute(const Example& e) {
 std::vector<double> NeuralNetwork::sigmoid(const std::vector<double>& x) {
     std::vector<double> result(x.size());
     for (unsigned int i = 0; i < x.size(); i++)
-        result[i] = 1 / (1 + exp(-x[i]));
+        result[i] =  (sqrt(pow(x[i], 2) + 1) - 1) / 2 + x[i];
     return result;
 }
 
@@ -190,7 +190,7 @@ std::vector<double> NeuralNetwork::sigmoid(const std::vector<double>& x) {
 std::vector<double> NeuralNetwork::personal_sigmoid(const std::vector<double>& x) {
     std::vector<double> result(x.size());
     for (unsigned int i = 0; i < x.size(); i++)
-        result[i] = x[i] / 2 + x[i];
+        result[i] = (sqrt(pow(x[i], 2) + 1) - 1) / 2 + x[i];
     return result;
 }
 std::vector<double> NeuralNetwork::sigmoid_prime(const std::vector<double>& x) {
