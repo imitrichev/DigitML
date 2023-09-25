d4783186405a06ebf83064038bdeea60c00330d3
vladt <vladt@DESKTOP-9ADNBFJ>
2023-09-25 20:00:53 +0300

    fix bugs in test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -21,8 +21,8 @@ TEST(FunctionTesting, sigmoid_test) {
 
 TEST(FunctionTesting, sigmoid_prime_test) {
 
-	std::vector<double> expectVector { 0.25, 0.196612, 0.104994, 4.53958e-05, 3.72008e-44, -nan};
-	std::vector<double> testVector { 0.0, 1.0, 2.0, 10.0, 100.0, 10000.0 };
+	std::vector<double> expectVector { 0.25, 0.196612, 0.104994, 4.53958e-05, 3.72008e-44};
+	std::vector<double> testVector { 0.0, 1.0, 2.0, 10.0, 100.0};
 	std::vector<double> result = sigmoid_prime(testVector);
 	
 	for (size_t i = 0; i < testVector.size(); i++) {
@@ -49,9 +49,9 @@ TEST(FunctionTesting, vectorize_label_test) {
 	std::vector<double> expectVector1{ 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 	std::vector<double> expectVector2{ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 	
-	std::vector<double> result0 = log(0);
-	std::vector<double> result1 = log(1);
-	std::vector<double> result2 = log(15);
+	std::vector<double> result0 = vectorize_label(0);
+	std::vector<double> result1 = vectorize_label(1);
+	std::vector<double> result2 = vectorize_label(15);
 
 	for (size_t i = 0; i < expectVector0.size(); i++) {
 		EXPECT_NEAR(result0[i], expectVector0[i], 1e-6);
@@ -65,8 +65,6 @@ TEST(FunctionTesting, vectorize_label_test) {
 		EXPECT_NEAR(result2[i], expectVector2[i], 1e-6);
 	}
 }
-
-vectorize_label
 
 
 int main(int argc, char** argv)
