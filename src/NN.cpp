6a01c6dced2f704947365d4991ab1dd3b5452458
Browwniass <95659478+Browwniass@users.noreply.github.com>
2023-09-25 22:13:58 +0300

    f

--- src/NN.cpp
+++ src/NN.cpp
@@ -190,7 +190,7 @@ std::vector<double> NeuralNetwork::sigmoid(const std::vector<double>& x) {
 std::vector<double> NeuralNetwork::personal_sigmoid(const std::vector<double>& x) {
     std::vector<double> result(x.size());
     for (unsigned int i = 0; i < x.size(); i++)
-        result[i] = /*(sqrt(pow(x[i], 2) + 1) - 1) / 2 + x[i]*/1 / (1 + exp(-x[i]));
+        result[i] = sqrt(pow(x[i], 2) + 1) / 2 + x[i];
     return result;
 }
 /*std::vector<double> NeuralNetwork::sigmoid_prime(const std::vector<double>& x) {
