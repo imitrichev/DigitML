745ef7f7cf1facab34eca56ec7e52b1c2dfa3477
Andrey <planchet2013@gmail.com>
2023-09-26 19:01:11 +0300

    refactorin main.cpp for tests

--- src/main.cpp
+++ src/main.cpp
@@ -56,7 +56,61 @@ void EndToEndTest() {
     printf("Accuracy on test data: %f\n", accuracy_test);
 }
 
-int main() {
+
+#ifdef TESTS
+#include "gtest/gtest.h"
+
+double prelu(double x, double alpha = 1.2) {
+
+    if (x >= 0) {
+        return x;
+    }
+    else {
+        return alpha * x;
+    }
+}
+
+std::vector<double> PReLU(const std::vector<double>& x) {
+    std::vector<double> result(x.size());
+    for (unsigned int i = 0; i < result.size(); i++) {
+        result[i] = prelu(x[i]);
+    }
+    return result;
+
+TEST(FunctionTesting, testMax1) {
+    EXPECT_NEAR(max(0.37, 0.29), 0.37, 1e-6);
+    EXPECT_NEAR(max(-0.52, -0.92), -0.52, 1e-6);
+    EXPECT_NEAR(max(0, 0.83), 0.83, 1e-6);
+}
+
+TEST(FunctionTesting, testMax2) {
+    EXPECT_NEAR(max(0.1, 0.0 * 0.1), 0.1, 1e-6);
+    EXPECT_NEAR(max(-0.1, 0.0 * (-0.1)), 0.0, 1e-6);
+    EXPECT_NEAR(max(0, 0.0 * 0), 0.0, 1e-6);
+}
+
+TEST(FunctionTesting, testReLUPos) {
+    std::vector<double> x1 = { 0.13, 0.23, 0.33, 0.43, 0.53 };
+    std::vector<double> right_x1 = { 0.13, 0.23, 0.33, 0.43, 0.53 };
+    ASSERT_EQ(ReLU(x1), right_x1);
+}
+
+TEST(FunctionTesting, testReLUMix) {
+    std::vector<double> x2 = { 0.05, -0.45, -0.24, 0.01, -0.99 };
+    std::vector<double> right_x2 = { 0.05, 0.0, 0.0, 0.01, 0.0 };
+    ASSERT_EQ(ReLU(x2), right_x2);
+}
+
+TEST(FunctionTesting, testReLUNeg) {
+    std::vector<double> x3 = { -0.75, -0.93, -0.38, -0.02, -0.63 };
+    std::vector<double> right_x3 = { 0.0, 0.0, 0.0, 0.0, 0.0 };
+    ASSERT_EQ(ReLU(x3), right_x3);
+}
+
+#endif
+
+
+int main(int argc, char** argv) {
     srand(time(NULL));
     Matrix<unsigned char> images_train(0, 0);
     Matrix<unsigned char> labels_train(0, 0);
@@ -83,11 +137,10 @@ int main() {
         EndToEndTest();
     }
 
-    printf("PreLu function test");
-    for (int i = 0; i <= 5; i++) {
-        printf("Тест: %d\n", i);
-        n.testPreluFunction();
-    }
+    #ifdef TESTS
+        ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+    #endif
     
     return 0;
 }
