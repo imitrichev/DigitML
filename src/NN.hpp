a26683dd65c15521664b4447bd4077c01211f73e
Anna <48104500+Anya-wUw@users.noreply.github.com>
2023-09-27 11:04:04 +0300

    Update NN.hpp
    heaviside public

--- src/NN.hpp
+++ src/NN.hpp
@@ -16,7 +16,6 @@ class NeuralNetwork {
                        weights2 = Matrix<double>(OUTPUT_SIZE, HIDDEN_SIZE);
 
         std::vector<double> sigmoid(const std::vector<double>& x);
-        std::vector<double> heaviside(const std::vector<double>& x);
         std::vector<double> sigmoid_prime(const std::vector<double>& x);
 
 		Matrix<double> weight_init(double max_weight, unsigned int width, unsigned int height);
@@ -29,6 +28,7 @@ class NeuralNetwork {
         NeuralNetwork();
         NeuralNetwork(const NeuralNetwork& rhs) = default;
         virtual ~NeuralNetwork() = default;
+	static std::vector<double> heaviside(const std::vector<double>& x);
 
         void train(
                 const unsigned int iterations,
